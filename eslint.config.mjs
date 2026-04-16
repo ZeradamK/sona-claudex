2b84d4a2b4728ce87ec8a8aecb77393932cfa33e
Zeradan <zeruliverpool@gmail.com>
2026-04-16 17:26:45 -0400

    Vendor voice library reference

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,13 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [".next/**", "node_modules/**", "next-env.d.ts", "public/**"]
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "next-env.d.ts",
+      "public/**",
+      "vendor/**"
+    ]
   }
 ];
 
